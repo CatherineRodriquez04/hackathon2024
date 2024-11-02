d580484ad995185ede104a4b4197e5faa125be22
yam yam <abetzw1@lsu.edu>
2024-11-02 15:41:13 -0500

    Prototype login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,25 @@
-//import Image from "next/image";
-"use client";
+"use client"
+import { Button } from "@/components/ui/button"
+import Image from "next/image";
+
+
 
 export default function login() {
   return (
-    <div className="container h-screen w-screen text-center mt-6">
-      <h1 className="h1">Login</h1>
+    <div className="container h-screen w-screen flex flex-row">
+      <div className="container text-center w-2/5 mt-40 mr-10">
+        <h1 className="h1">Login</h1>
+        <div className="flex flex-col gap-5 mt-5 border border-black border-1 rounded-md p-5">
+        <Button>Continue with Google</Button>
+        <Button>Continue with email</Button>
+        <text className="">Trouble logging in?</text>
+        </div>
+      </div>
+      <div className="w-3/5 bg-cover">
+        <Image src="/assets/login-img.jpg" alt="Image" width="500" height="500"/>
+      </div>
     </div>
+    
+    
   );
 }
