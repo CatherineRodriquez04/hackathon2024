53d802e962c04a8edea85eda8d6b755587154de2
Kameron Arceneaux <kameron.arceneaux@Outlook.com>
2024-11-02 14:54:47 -0500

    fixed issue with the login not page not loading

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,35 +8,33 @@ import Nav from "./Nav";
 import MobileNav from "./MobileNav";
 
 const Header = () => {
-    return (
-        <header className="py-8 xl:py-12 text-black">
-            <div className="mx-8 flex justify-between items-center">
-                {/* logo */}
-                <Link href="/">
-                <h1 className="text-4xl fonts-semibold hover:scale-110">
-                    Demo<span className="text-accent">.</span>
-                </h1>
-                </Link>
+  return (
+    <header className="py-8 xl:py-12 text-black">
+      <div className="mx-8 flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl fonts-semibold hover:scale-110">
+            Demo<span className="text-accent">.</span>
+          </h1>
+        </Link>
 
+        {/* desktop nav & hire me button */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/login">
+            <Button>Login</Button>
+          </Link>
+        </div>
 
-                {/* desktop nav & hire me button */}
-                <div className="hidden xl:flex items-center gap-8">
-                    <Nav />
-                    <Link href="../app/login/login.jsx">
-                        <Button>Login</Button>
-                    </Link>
-                </div>
-
-                {/* mobile nav */}
-                <div className="xl:hidden">
-                    <MobileNav />
-                </div>
-
-            </div>
-            {/* border */}
-            <div className="mt-2 border border-black/10"></div>
-        </header>
-    );
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+      {/* border */}
+      <div className="mt-2 border border-black/10"></div>
+    </header>
+  );
 };
 
-export default Header
+export default Header;
