4d081da4c789054290ad93803d3fc40080464866
Kameron Arceneaux <kameron.arceneaux@Outlook.com>
2024-11-03 00:54:47 -0500

    made login more responsive

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,11 +3,11 @@ import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
 import Image from "next/image";
 import { auth } from "../firebaseConfig";
 import { useRouter } from "next/navigation";
+import { FcGoogle } from "react-icons/fc"; // Import Google icon
 import { Button } from "@/components/ui/button";
 
 export default function Login() {
   const router = useRouter();
-
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = async () => {
@@ -27,20 +27,28 @@ export default function Login() {
   };
 
   return (
-    <div className="container h-screen w-screen flex flex-row">
-      <div className="container text-center w-2/5 mt-40 mr-10">
-        <h1 className="h1">Login</h1>
-        <div className="flex flex-col gap-5 mt-5 border border-black border-1 p-5">
-          <Button onClick={handleGoogleSignIn}>Continue with Google</Button>
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="flex flex-col items-center justify-center w-full md:w-2/4 p-4">
+        <h1 className="text-3xl font-bold mb-6">Login</h1>
+        <div className="flex flex-col gap-2 border border-gray-300 p-5 w-full max-w-sm rounded-lg shadow-lg">
+          <Button
+            className="flex items-center justify-center gap-2"
+            onClick={handleGoogleSignIn}
+          >
+            <FcGoogle size={20} /> Continue with Google
+          </Button>
         </div>
       </div>
-      <div className="w-3/5 bg-cover">
-        <Image
-          src="/assets/login-img.jpg"
-          alt="Image"
-          width="500"
-          height="500"
-        />
+      <div className="w-full md:w-3/5 h-64 md:h-full bg-cover">
+        <div className="relative h-full">
+          <Image
+            src="/assets/KarlTemp.jpg"
+            alt="Login Image"
+            layout="fill"
+            objectFit="contain"
+            className="rounded-t-lg md:rounded-none"
+          />
+        </div>
       </div>
     </div>
   );
